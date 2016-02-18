fe8dfbde7ee2027f8bd9bf933880c4f7b173661e
karandesai-96 <karandesai_96@live.com>
2016-02-18 07:54:12 +0530

    Separates out the code of fetching headings to separate method.
    - fetch_table_headings takes in the soup and returns a list of
      headings of the table.

--- atomatic.py
+++ atomatic.py
@@ -1,14 +1,20 @@
 from utils import soup_utils as su
+import unicodedata
 
 
 def fetch_atomic_dataset():
     url = "http://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl"
 
     physics_nist_soup = su.get_soup_from_url(url)
-
-    for header in physics_nist_soup.find_all("th", attrs={'valign': 'bottom'}):
-        print "HEADER ......."
-        print header.text
-        print type(header.text)
+    headings = fetch_table_headings(physics_nist_soup)
 
     return
+
+
+def fetch_table_headings(soup):
+    headings = list()
+
+    for heading in soup.find_all("th", attrs={'valign': 'bottom'}):
+        headings.append(heading.text)
+
+    return headings
