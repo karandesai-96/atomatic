8b11940c856ec84fc1f737b21a26beca03eb71c6
karandesai-96 <karandesai_96@live.com>
2016-02-19 16:46:43 +0530

    Initializes pandas DataFrame, removes fetch_heading.
    - fetch_headings is no longer required as Pandas DataFrame
      has different schema than that on website.

--- atomatic.py
+++ atomatic.py
@@ -1,24 +1,16 @@
 from utils import soup_utils as su
 from utils import text_utils as tu
+import pandas as pd
 
 
 def fetch_atomic_dataset():
     url = "http://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl"
 
     physics_nist_soup = su.get_soup_from_url(url)
-    headings = fetch_table_headings(physics_nist_soup)
+    headings = ["Z", "Symbol", "Nucleons", "Relative Atomic Mass", "Isotopic \
+                Composition", "Standard Atomic Weight", "Notes"]
 
+    atomic_dataset = pd.DataFrame(columns=headings)
+
+    rows = physics_nist_soup.find_all("tr")
     return
-
-
-def fetch_table_headings(soup):
-    headings = list()
-
-    for heading in soup.find_all("th", attrs={'valign': 'bottom'}):
-        headings.append(heading.text)
-
-    for i in range(len(headings)):
-        headings[i] = tu.unicode_to_utf8(headings[i])
-        headings[i] = tu.beautify_text(headings[i])
-
-    return headings
