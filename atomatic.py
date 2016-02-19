975fcefd46241b21f2f00982cfdfa6319c1f4f7a
karandesai-96 <karandesai_96@live.com>
2016-02-19 18:31:43 +0530

    Automatically caches request respose content to html file.
    - The html files are stored in outdump/ directory and
      can be cleaned up manually. Saves waiting time of request
      response.

--- atomatic.py
+++ atomatic.py
@@ -1,12 +1,18 @@
 from utils import soup_utils as su
 from utils import text_utils as tu
+import os
 import pandas as pd
 
 
 def fetch_atomic_dataset():
     url = "http://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl"
 
-    physics_nist_soup = su.get_soup_from_url(url)
+    if os.path.isfile("mainsoup.html"):
+        physics_nist_soup = su.get_soup_from_file("mainsoup.html")
+    else:
+        physics_nist_soup = su.get_soup_from_url(url)
+        su.store_content_from_soup(physics_nist_soup, filename="mainsoup.html")
+
     headings = ["Z", "Symbol", "Nucleons", "Relative Atomic Mass", "Isotopic \
                 Composition", "Standard Atomic Weight", "Notes"]
 
