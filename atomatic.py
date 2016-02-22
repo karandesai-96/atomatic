825476a4ac6882f3eff1b61d5dc89d2be225acde
karandesai-96 <karandesai_96@live.com>
2016-02-22 23:20:44 +0530

    Adds docstrings in first two methods of atomatic.py

--- atomatic.py
+++ atomatic.py
@@ -5,19 +5,36 @@ import pandas as pd
 
 
 def fetch_atomic_dataset():
+    """
+    * Fetches the atomic dataset available at given url in html tabular format
+      and parses it into a pandas DataFrame.
+    * It uses methods from soup_utils.py and forms a BeautifulSoup using request
+      response content of url.
+    * It iteratively goes through each row (`<tr>` tag) and makes calls to
+      helper function, `fetch_row` written below to parse the data and
+      categorize it properly.
+
+    :return: data_frame: pandas DataFrame containing the parsed atomic dataset
+    """
+
     url = "http://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl"
 
+    # writes request response content to a file and uses it subsequently,
+    # done for speeding up and avoiding multiple requests while debugging
     if os.path.isfile("outdump/mainsoup.html"):
         soup = su.get_soup_from_file("mainsoup.html")
     else:
         soup = su.get_soup_from_url(url)
         su.store_content_from_soup(soup, filename="mainsoup.html")
 
+    # each will form a column of data frame
     headings = ["Z", "Symbol", "Nucleons", "Relative Atomic Mass",
                 "Isotopic Composition", "Standard Atomic Weight", "Notes"]
 
     data_frame = pd.DataFrame(columns=headings)
 
+    # iterate through each row of the table and obtain a dictionary having
+    # keys same as those mentioned in headings list
     for row in soup.find_all("tr"):
         df_record = fetch_row(row)
         if df_record is not None:
@@ -27,14 +44,26 @@ def fetch_atomic_dataset():
 
 
 def fetch_row(row):
+    """
+    Fetches the data by parsing a single row. Collection is done by making
+    various calls to methods listed below which parse single entry of the row
+    and add appropriate key value pair in the dictionary.
+
+    :param row: BeautifulSoup tag for a row in table. Contains `<td>` tags
+                having the needed data.
+    :return: df_record: a record for single isotope of any element to be
+                appended at the end of the data frame. If row doesn't have
+                required data (blue line, or stray) then NoneType is returned.
+    """
     df_record = dict()
 
     row_data = row.find_all("td")
 
-    # ignoring the row containing blue line and other stray rows
+    # ignore the row containing blue line and other stray rows
     if len(row_data) < 3 or row_data[0].has_attr("colspan"):
         return None
 
+    # parse and add all the entries of the record one by one
     row_data, df_record = parse_z_and_symbol(row_data, df_record)
     df_record = parse_nucleons(row_data, df_record)
     df_record = parse_relative_atomic_mass(row_data, df_record)
