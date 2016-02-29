526f171c8f5bf8f0508f09336f9c9c43c2aa3dd0
karandesai-96 <karandesai_96@live.com>
2016-02-29 18:02:07 +0530

    Writes test for dataframe_to_sql_table of db_utils.py
    - The test calls the method to create a sample table of two
      columns and three rows and assertion is made whether the
      table exists, to confirm working of the method.
    
    - test_db_credentials.json serve for travis CI currently.

---
+++ utils/tests/test_db.py
@@ -0,0 +1,32 @@
+import os
+import json
+import pandas as pd
+import sqlalchemy as sqlalc
+import utils.db_utils as dbu
+
+test_dataframe = pd.DataFrame({"Int": [1, 2, 3], "Char": ['a', 'b', 'c']})
+
+
+def test_dataframe_to_sql_table():
+    # obtain test credentials
+    json_data = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                  "test_db_credentials.json"), 'r')
+    cred = json.loads(json_data.read())
+    json_data.close()
+
+    # create sqlalchemy engine
+    engine = sqlalc.create_engine(cred[u'engine'] + '://' + cred[u'user'] + ':'
+                                  + cred[u'password'] + '@' + cred[u'host'] +
+                                  ':' + cred[u'port'] + '/' + cred[u'db'])
+
+    # drop 'Test Table' if exists (might happen if test failed earlier)
+    if engine.dialect.has_table(engine, "Test Table"):
+        test_table = sqlalc.Table("Test Table", sqlalc.MetaData(engine))
+        test_table.drop()
+
+    dbu.dataframe_to_sql_table(test_dataframe, "Test Table")
+    assert engine.dialect.has_table(engine, "Test Table")
+
+    # teardown
+    test_table = sqlalc.Table("Test Table", sqlalc.MetaData(engine))
+    test_table.drop()
